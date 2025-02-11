136a2c6cf23fdede061ebdacdea17d1f7bfe503a
Sultanbek02173 <sultan.02173@gmail.com>
2025-02-11 15:49:37 +0600

    first init

---
+++ src/app/routes/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header className='container header'>
+        <button className='row header__container_button'></button>
+
+        <div className='header__icons'>
+          <img src="" alt="" />
+          <h1 className='header__icons_title'>
+            <p className='header__icons_title_text'></p>
+          </h1>
+        </div>
+      </header>
+    </>
+  )
+}
+
+export default App
